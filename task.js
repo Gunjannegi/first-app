31e3070095b2e0e71d40fb6eeb8e3b0c6d532e38
Your Name <gunjan.negi.5249@gmail.com>
2024-05-25 14:19:05 +0530

    Routers and Filters

--- task.js
+++ task.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop')
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended:false}));
@@ -12,16 +14,9 @@ app.use(bodyParser.urlencoded({extended:false}));
 //     res.send( { key1: 'value' })
 // })
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add</button></form>')
-});
-
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello from Express!</h1>')
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
 })
-app.listen(4001)
+app.listen(4000);
