840e39ae8fd495722491de4a6fb9f248697e9dfb
GunjanLunaraSol <gunjan.negi@lunarasolutions.in>
2024-06-01 20:06:57 +0530

    Serving HTML,CSS and JS (Views)

--- task.js
+++ task.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop')
+const shopRoutes = require('./routes/shop');
+const contactRoutes = require('./routes/contactUs');
+const successRoutes = require('./routes/success');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended:false}));
@@ -13,10 +16,13 @@ app.use(bodyParser.urlencoded({extended:false}));
 //     console.log('In the another middleware');
 //     res.send( { key1: 'value' })
 // })
-
+app.use(express.static(path.join(__dirname,'public')))
 app.use('/admin',adminRoutes);
+app.use(contactRoutes);
 app.use(shopRoutes);
+app.use(successRoutes);
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found</h1>')
+    // res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
 })
 app.listen(4000);
